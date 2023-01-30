aed5d6bd3ce630ec448dce9cfe73d7abbe1d505f
bjick84 <bjick@web.de>
2023-01-30 11:16:44 +0100

    out this

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,9 +3,16 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-
+        
         Cat cat = new Cat();    // Instanziierung class --> objekt
         System.out.println(cat);
+        cat.tellYourAddress();
+        
+        System.out.println("---------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
     }
 
   
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,4 +2,7 @@ package com.cc.java;
 
 public class Cat {
     
+    public void tellYourAddress() {
+        System.out.println(this);   
+    }
 }
